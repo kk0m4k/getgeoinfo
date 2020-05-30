7f747b13962d0d63e677b7011f7aa8d6c9f61961
kk0m4k <42513698+kk0m4k@users.noreply.github.com>
2020-05-30 23:13:03 +0900

    Update getgeoinfo.py

--- getgeoinfo.py
+++ getgeoinfo.py
@@ -2,6 +2,7 @@ import ssl
 import certifi
 import geopy
 import argparse
+import pandas as pd
 from geopy.geocoders import Nominatim
 
 class Getgeoinfo():
@@ -11,11 +12,14 @@ class Getgeoinfo():
     """
 
     def __init__(self, address=None, timeout=None, ssl_verify_flag=False):
-        self.address = args.address
-        self.timeout = args.timeout
-        self.ssl_verify_flag = args.ssl_verify_flag
+        self.pasrge_argument()
 
-        print(args)
+        self.address = self.args.address
+        self.timeout = self.args.timeout
+        self.ssl_verify_flag = self.args.ssl_verify_flag
+        self.bulk_file = self.args.bulk_file
+
+        print(self.args)
 
     def ssl_verify(self, flag=False):
         if not flag:
@@ -33,33 +37,63 @@ class Getgeoinfo():
                       'AppleWebKit/537.36 (KHTML, like Gecko) ' \
                       'Chrome/71.0.3578.98 Safari/537.36'
 
-        try:
-            _n = Nominatim(user_agent=_user_agent, timeout=self.timeout)
-            _x = _n.geocode(self.address)
-        except Exception as e:
-            print(str(e))
-            return None
+        _n = Nominatim(user_agent=_user_agent, timeout=self.timeout)
 
-        if not _x:
-            return None
+        if self.address:
+            try:
+                _x = _n.geocode(self.address)
+            except Exception as e:
+                print(str(e))
+                return None
+    
+            if not _x:
+                return None
+    
+            return {'address':_x.address.encode('utf-8'), 
+                    'lat':_x.latitude, 'long':_x.longitude}
 
-        return {'address':_x.address.encode('utf-8'), 
-                'lat':_x.latitude, 'long':_x.longitude}
+        elif self.bulk_file:
+            try:
+                _df = pd.read_csv(self.bulk_file)
+                _df['location'] = _df['Address'].apply(_n.geocode)
+
+                _df['latitude'] = _df['location'].apply(lambda loc: 
+                                  loc.latitude if loc.latitude else None) 
+                _df['longitude'] = _df['location'].apply(lambda loc:
+                                   loc.longitude if loc.longitude else None) 
+
+                _df.to_csv(self.bulk_file,
+                            sep=',', 
+                            index=False)
+                return _df.to_dict('records')
+
+            except Exception as e:
+                print(str(e))
+                return None
+    
+    def pasrge_argument(self):
+        arg_parse = argparse.ArgumentParser(description='python3 getgeoinfo.py -i address')
+        arg_parse.add_argument('-a', '--address', type=str, action='store',
+                                required=False, help='input address to be search')
+        arg_parse.add_argument('-t', '--timeout', type=int, default=10, 
+                                required=False, help='setting TIMEOUT value')
+        arg_parse.add_argument('-s', '--ssl_verify_flag', default=False, action='store_true',
+                                required=False, help='setting ssl verify flag')
+        arg_parse.add_argument('-b', '--bulk_file', default=False, action='store',
+                                required=False, help='processing csv file')
+        self.args = arg_parse.parse_args()
+        self.args = arg_parse.parse_args()
 
 if __name__ == "__main__":
-    arg_parse = argparse.ArgumentParser(description='python3 getgeoinfo.py -i address')
-    arg_parse.add_argument('-a', '--address', type=str, action='store',
-                            required=False, help='input address to be search')
-    arg_parse.add_argument('-t', '--timeout', type=int, default=10, 
-                            required=False, help='setting TIMEOUT value')
-    arg_parse.add_argument('-s', '--ssl_verify_flag', default=False, action='store_true',
-                            required=False, help='setting ssl verify flag')
-    args = arg_parse.parse_args()
-
     o_geo = Getgeoinfo()
     geo_result = o_geo.get_latlong()
+
     if not geo_result:
         print('Failed when searching geo location for this address')
+        exit(0)
 
-    print(geo_result['address'].decode('utf-8'),
-          geo_result['lat'], geo_result['long'])
+    if not o_geo.bulk_file:
+        print(geo_result['address'].decode('utf-8'),
+              geo_result['lat'], geo_result['long'])
+    else:
+        print(geo_result)
